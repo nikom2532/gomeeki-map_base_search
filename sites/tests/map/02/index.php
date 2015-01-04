ab89014362c4d88dbfd5494e5624056d08b70ec8
Arming Huang <arming@buildthedot.com>
2015-01-04 16:41:23 +0700

    test: google map: add the action for search

--- sites/tests/map/02/index.php
+++ sites/tests/map/02/index.php
@@ -30,12 +30,27 @@
 				outline: none;
 				box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
 			}
+			#city-label{
+				position: absolute;
+				top: 5%;
+				left: 50%;
+				z-index: 999;
+				border: 2px solid #000;
+				background-color: #fff;
+				display: none;
+			}
+			#city-text{
+				position: relative;
+				color: #000;
+				margin: 5px;
+			}
 		</style>
 		<!-- <script type="text/javascript"
 		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAzxoqIy0F2_2xhgXOgCZHSH83Mtw4Kujg"></script> -->
 
-		<script type="text/javascript"
-		src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
+		
+		<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
 
 		<script type="text/javascript">
 			function initialize() {
@@ -53,7 +68,8 @@
 					if($_POST["search_cityname"] == "yes"){
 ?>
 						setMarker(map);
-						
+						$("#city-label").css("display", "block");
+						$("#city-text").html("<?php echo $_POST["cityname"]; ?>");
 <?php
 					}
 				}
@@ -101,9 +117,16 @@
 		</script>
 	</head>
 	<body>
+		<div id="city-label">
+			<div id="city-text"></div>
+		</div>
 		<div id="map-canvas"></div>
 		<form class="form" name="form" id="form" method="post">
-			<input type="text" class="pac-input" value="" placeholder="City name" />
+			<input type="text" name="cityname" class="pac-input" value="<?
+				if(isset($_POST["cityname"])){
+					echo $_POST["cityname"];
+				}
+			?>" placeholder="City name" />
 			<input type="hidden" name="search_cityname" value="yes" />
 		</form>
 	</body>
